43ae7ab5086a1de0eaee3b47e16db3e63d14a283
Aman Konchgamey <amanmaxwel@gmail.com>
2023-08-09 09:02:04 +0530

    admin-home

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   const {data: session} = useSession()
   return(
       <Layout>
+        <div>
         <div className="text-blue-900 flex justify-between">
           <h2>Hello, <br /><b>{session?.user?.name}</b></h2>
           <div className="flex bg-gray-300 text-black gap-1 h-6 w-36 md:w-48 rounded-lg overflow-hidden">
@@ -13,6 +14,11 @@ export default function Home() {
             <span className="px-2 truncate">{session?.user?.name}</span>
           </div>
         </div>
+        <div className="mt-8 max-w-800 md:ml-32">
+          <img src="https://www.datapine.com/blog/wp-content/uploads/2019/08/revenue-per-sales-rep.png" alt='' />
+        </div>
+        </div>
+        
       </Layout>
     )
 
